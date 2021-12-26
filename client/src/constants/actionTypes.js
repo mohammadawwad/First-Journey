737180f53996cb6bed95a1f131a3a75976aa210b
mohammadawwad <mohd-awwad@hotmail.com>
2021-12-25 19:23:21 -0800

    pls work i think i broke git while trying to deploy to heroku

---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,7 @@
+//constants for actions rather than strings
+
+export const CREATE = "CREATE";
+export const UPDATE = "UPDATE";
+export const DELETE = "DELETE";
+export const LIKE = "LIKE";
+export const FETCH_ALL = "FETCH_ALL";
