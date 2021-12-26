595757ab7618e7d7f880293c7ba3be0f54505d3f
mohammadawwad <mohd-awwad@hotmail.com>
2021-12-25 20:26:44 -0800

    pls

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 app.use("/posts", postRoutes)
 
-//api heroku test
 app.get('/', (req, res) => {
     res.send("Hello to First Jouney API");
 });
