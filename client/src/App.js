94e6aaf81b280d7363973882535829ea15e7bedd
mohammadawwad <mohd-awwad@hotmail.com>
2021-12-27 22:11:50 -0800

    cleaned app.js created routes and created front end for sign in and profile info

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import React, {useState, useEffect} from "react";
-import {Container, AppBar, Typography, Grow, Grid} from "@material-ui/core";
-import {useDispatch} from "react-redux"
-import {getPosts} from "./actions/postsAction"
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
-import useStyles from "./styles";
+import React from "react";
+import {Container} from "@material-ui/core";
+import Navbar from "./components/Navbar/Navbar";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
 
 //add img for app
@@ -12,35 +11,22 @@ import useStyles from "./styles";
 //import journey from "./images/journey.png"
 
 const App = () => {
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    const [currentId, setCurrentId] = useState(null);
 
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
 
     return (
-        <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">First Journey</Typography>
-                {/* <img className={classes.image} src={journey} alt="journey" height="60"/> */}
-            </AppBar>
+        <BrowserRouter>
             
-            <Grow in>
-                <Container>
-                    <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId}/>
-                        </Grid>
+            <Container maxWidth="lg">
+                <Navbar/>
 
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/auth" exact component={Auth} />
+                </Switch>
+            </Container>
+
+        </BrowserRouter>
+
     );
 }
 
