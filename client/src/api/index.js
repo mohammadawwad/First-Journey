7b0d6b0114519d848dced7e09f9ea4fac2b29e87
mohammadawwad <mohd-awwad@hotmail.com>
2021-12-31 00:15:13 -0800

    saving last change of the night, request keeps pending

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -16,7 +16,7 @@ API.interceptors.request.use((req) => {
 // const url = "https://first-journey.herokuapp.com/posts";
 
 export const fetchPosts = () => API.get("/posts");
-export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.searchTitle || "none"}&tags=${searchQuery.tags}`);
+export const fetchPostsBySearch = (searchQuery) => API.get(`/posts/searchTitle?searchQuery=${searchQuery.searchTitle || "none"}&tags=${searchQuery.tags}`);
 export const createPost = (newPost) => API.post("/posts", newPost);
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
