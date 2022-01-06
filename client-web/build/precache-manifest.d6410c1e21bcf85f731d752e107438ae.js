3ac9a842b135d7addc0474377e59965bbb2e52e9
mohammadawwad <mohd-awwad@hotmail.com>
2022-01-05 19:44:27 -0800

    deployed ffront end and made some final changes

---
+++ client-web/build/precache-manifest.d6410c1e21bcf85f731d752e107438ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d742817a0c71b4a5787fbbb6f77c619c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51623a349505371dfa26",
+    "url": "/static/css/main.95c04b65.chunk.css"
+  },
+  {
+    "revision": "5cee66f6358cb20ce48a",
+    "url": "/static/js/2.6919591a.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.6919591a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51623a349505371dfa26",
+    "url": "/static/js/main.9c6f8fc9.chunk.js"
+  },
+  {
+    "revision": "c3118a4147a919f668b9",
+    "url": "/static/js/runtime-main.0bd55240.js"
+  }
+]);
