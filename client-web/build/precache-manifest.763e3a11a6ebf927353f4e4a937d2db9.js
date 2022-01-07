3f64a18fe06d76e68499690bafc97a6201a3124c
mohammadawwad <mohd-awwad@hotmail.com>
2022-01-07 01:34:00 -0800

    all is googd and working might publish in the morning

---
+++ client-web/build/precache-manifest.763e3a11a6ebf927353f4e4a937d2db9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99e51def0a3e621211ad13c1a4f0fcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e793b23d24b19ed7b0f1",
+    "url": "/static/css/main.95c04b65.chunk.css"
+  },
+  {
+    "revision": "36db401a3b1bdb7dbbb0",
+    "url": "/static/js/2.71f67aa0.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.71f67aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e793b23d24b19ed7b0f1",
+    "url": "/static/js/main.3b716ada.chunk.js"
+  },
+  {
+    "revision": "c3118a4147a919f668b9",
+    "url": "/static/js/runtime-main.0bd55240.js"
+  },
+  {
+    "revision": "b8575c38c268e97484ff65aa50951d66",
+    "url": "/static/media/logo.b8575c38.png"
+  }
+]);
